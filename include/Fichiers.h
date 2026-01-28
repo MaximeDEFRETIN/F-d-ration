0a9d94da2c07a2a1831f7cd4fe17dadf7ff0de7f
Maxime <maxime.defretin@laposte.ent>
2026-01-28 18:26:25 +0100

    Création de la classe Fichiers

---
+++ include/Fichiers.h
@@ -0,0 +1,19 @@
+#ifndef FICHIERS_H
+#define FICHIERS_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Fichiers {
+    public:
+        //Fichiers();
+        //Fichiers();
+
+    protected:
+
+    private:
+};
+
+#endif // FICHIERS_H
---
+++ src/Fichiers.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <string>
+#include <Fichiers.h>
+
+using namespace std;
